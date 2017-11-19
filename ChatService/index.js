227d2fbf58ddd584e14b0e9484dcc3b7886bb743
Fadhil Imam Kurnia <fadhilimamk@gmail.com>
2017-11-20 01:07:24 +0700

    ChatService: add basic sctructure for chat service

---
+++ ChatService/index.js
@@ -0,0 +1,45 @@
+// ========================== OLRIDE Chat Service =======================
+const express        = require('express');
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+const port           = 8123;
+const app            = express();
+
+// Menyimpan identitas (token FCM) dari masing-masing pengguna yang sedang online
+// Menerima request dari user A untuk chat ke user B, lalu membuat request ke FCM untuk pengiriman pesan ke token FCM user B.
+// Menyimpan ke basis data history chat dari seorang pemesan dan seorang driver. Misalkan A pernah memesan driver B. Jika suatu saat A akan memesan lagi ke driver B, maka kotak chat menampilkan chat yang dilakukan pada pemesanan sebelumnya.
+
+app.get('/', function(request, response) {
+    response.send("Olride!");
+});
+
+// Memberikan daftar driver sedang mencari pesanan
+app.get('/driver/online', function(request, response) {
+    response.json(
+        [
+            {
+                token: 'asdasdasdasdasdasdasd',
+                token_fcm: 'asdasdasdasdasdasd'
+            },
+            {
+                token: 'asdasdasdasdasdasdasd',
+                token_fcm: 'asdasdasdasdasdasd'
+            },
+            {
+                token: 'asdasdasdasdasdasdasd',
+                token_fcm: 'asdasdasdasdasdasd'
+            }
+        ]
+    );
+});
+
+// Menerima request untuk mengirimkan pesan ke :target
+app.post('/message/send/:target', function(request, response) {
+    var target = request.params.target;
+    response.send("sending tp " + target);
+});
+
+
+app.listen(port, () => {
+    console.log('Olride Chat Service is active on ' + port);
+});
